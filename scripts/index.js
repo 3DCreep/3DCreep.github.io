decda0a7750b3e7503f71226229ab1b960b92c82
3DCreep <145836003+3DCreep@users.noreply.github.com>
2023-11-01 11:59:01 +0600

    Theme button

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
-alert("test");
+$("#theme_btn").on("click", function () {
+  const html = $("#html");
+  if (html.attr("data-theme") == "light") {
+    html.attr("data-theme", "dark");
+  } else {
+    html.attr("data-theme", "light");
+  }
+});
