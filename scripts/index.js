3d229160f7e73881d02c0cbeb59e3562bc85e830
3DCreep <145836003+3DCreep@users.noreply.github.com>
2023-11-04 14:53:48 +0600

    Imported another page, new sections

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 $("#theme_btn").on("click", function () {
   const html = $("#html");
+  const indit = $(".themebtn_ind");
   if (html.attr("data-theme") == "light") {
     html.attr("data-theme", "dark");
+    indit.css("top", "50%");
   } else {
     html.attr("data-theme", "light");
+    indit.css("top", "0%");
   }
 });
