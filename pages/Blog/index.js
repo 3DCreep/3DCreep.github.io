5aa930c7aff49a543131fde68ee477e88bcf32f2
3DCreep <145836003+3DCreep@users.noreply.github.com>
2023-12-02 20:07:16 +0600

    Header

---
+++ pages/Blog/index.js
@@ -0,0 +1,4 @@
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import html from "highlight.js/lib/languages/vbscript-html";
+import scss from "highlight.js/lib/languages/scss";
