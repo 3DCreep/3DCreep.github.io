f27eccac8cd79676c0c0440b1525ee9758ac1019
3DCreep <145836003+3DCreep@users.noreply.github.com>
2023-12-04 19:32:02 +0600

    Uh... Yeah

---
+++ pages/Blog/blogs/script.js
@@ -0,0 +1,8 @@
+// import hljs from "../../../node_modules/highlight.js/lib/core";
+// import javascript from "../../../node_modules/highlight.js/lib/languages/javascript";
+// import html from "../../../node_modules/highlight.js/lib/languages/vbscript-html";
+// import scss from "../../../node_modules/highlight.js/lib/languages/scss";
+// hljs.registerLanguage("javascript", javascript);
+// hljs.registerLanguage("html", html);
+// hljs.registerLanguage("scss", scss);
+// hljs.highlightAll();
