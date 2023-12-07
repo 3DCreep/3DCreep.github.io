fb54b25a930ed079276dc098db4e136207958932
3DCreep <145836003+3DCreep@users.noreply.github.com>
2023-12-07 16:04:31 +0600

    2 pages (mostly) done

--- pages/Blog/blogs/script.js
+++ pages/Blog/blogs/script.js
@@ -6,3 +6,14 @@
 // hljs.registerLanguage("html", html);
 // hljs.registerLanguage("scss", scss);
 // hljs.highlightAll();
+$(document).ready(function () {
+  textblock = $("pre code").html();
+  var lines = textblock.split("\n");
+  lines.splice(0, 1);
+  var newtext = lines.join("\n");
+  $("pre code").html(newtext);
+
+  $("pre code").each(function (i, block) {
+    hljs.highlightBlock(block);
+  });
+});
