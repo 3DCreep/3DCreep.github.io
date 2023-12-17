bb221c145ddf1387f72df4338a26ed80814c5bad
3DCreep <145836003+3DCreep@users.noreply.github.com>
2023-12-17 12:38:44 +0600

    New page, cookies

--- pages/Blog/blogs/script.js
+++ pages/Blog/blogs/script.js
@@ -1,27 +1,6 @@
-// import hljs from "../../../node_modules/highlight.js/lib/core";
-// import javascript from "../../../node_modules/highlight.js/lib/languages/javascript";
-// import html from "../../../node_modules/highlight.js/lib/languages/vbscript-html";
-// import scss from "../../../node_modules/highlight.js/lib/languages/scss";
-// hljs.registerLanguage("javascript", javascript);
-// hljs.registerLanguage("html", html);
-// hljs.registerLanguage("scss", scss);
 hljs.highlightAll();
-// $(document).ready(function () {
-//   textblock = $("pre code").html();
-//   var lines = textblock.split("\n");
-//   lines.splice(0, 1);
-//   var newtext = lines.join("\n");
-//   $("pre code").html(newtext);
 
-//   $("pre code").each(function (i, block) {
-//     hljs.highlightBlock(block);
-//   });
-// });
-function open_nav() {
-  $(".main_nav").toggleClass("open_nav");
-  $(".nav_logo").toggleClass("open_navlogo");
-  $(".bar").toggleClass("active_bar");
-}
+let code = document.querySelector("html");
 document.addEventListener("DOMContentLoaded", () => {
   const progress = document.getElementById("progressbar");
   window.addEventListener("scroll", () => {
@@ -32,3 +11,57 @@ document.addEventListener("DOMContentLoaded", () => {
     progress.setAttribute("perc", Math.round(progress.value) + "%");
   });
 });
+function open_nav() {
+  $(".main_nav").toggleClass("open_nav");
+  $(".nav_logo").toggleClass("open_navlogo");
+  $(".bar").toggleClass("active_bar");
+}
+
+code.setAttribute("data-theme", getCookie("data-theme"));
+// $("html").attr("data-theme") = getCookie();
+$("#cookie-button").on("click", () => {
+  createCookie("data-theme", "ayu", 20);
+  console.log(getCookie("data-theme"), "< = The cookie click");
+});
+console.log(getCookie("data-theme"));
+function createCookie(name, val, expTime) {
+  const date = new Date();
+  date.setTime(date.getTime() + expTime * 24 * 3600 * 1000);
+  let expire = "expires=" + date.toUTCString();
+  document.cookie = `${name}=${val}; ${expire}; path=/`;
+  console.log(document.cookie);
+}
+
+// function createSimpleCookie(val, expTime) {
+//   const date = new Date();
+//   date.setTime(date.getTime() + expTime * 24 * 3600 * 1000);
+//   let expire = "expires=" + date.toUTCString();
+//   document.cookie = `${val}; ${expire}; path=/`;
+//   console.log(document.cookie);
+// }
+$("#sup_buttob").on("click", () => {
+  console.log(getCookie("data-theme"), "< = The cookie");
+});
+console.log(getCookie("data-theme"), "< = The cookie other");
+function getCookie(name) {
+  const cokDecode = decodeURIComponent(document.cookie);
+  const cokArray = cokDecode.split(";");
+  var resultCok = null;
+
+  cokArray.forEach((el) => {
+    console.log(el);
+    if (el.indexOf(name) == 0) {
+      console.log(el, "< = element");
+      resultCok = el.substring(name.length + 1);
+      console.log(name, "< = Name");
+      console.log(resultCok, "< = Value");
+    }
+  });
+  return resultCok;
+}
+function deleteCookie(name) {
+  createCookie(name, null, null);
+}
+
+// document.cookie = `site_theme=${$("html").attr("data-theme")}; `;
+// console.log(document.cookie);
--- pages/Blog/main.js
+++ pages/Blog/main.js
@@ -6,7 +6,7 @@ function open_nav() {
 
 document.getElementById("email_link").addEventListener("click", function () {
   // Get the text you want to copy
-  let text = "boom";
+  let text = "AlexST_frwrk@proton.me";
 
   // Use the Clipboard API to write the text to the clipboard
   navigator.clipboard
@@ -18,3 +18,31 @@ document.getElementById("email_link").addEventListener("click", function () {
       console.error("Sorry, an error occured while copying", err);
     });
 });
+let code = document.querySelector("html");
+code.setAttribute("data-theme", getCookie("data-theme"));
+function createCookie(name, val, expTime) {
+  const date = new Date();
+  date.setTime(date.getTime() + expTime * 24 * 3600 * 1000);
+  let expire = "expires=" + date.toUTCString();
+  document.cookie = `${name}=${val}; ${expire}; path=/`;
+  console.log(document.cookie);
+}
+function getCookie(name) {
+  const cokDecode = decodeURIComponent(document.cookie);
+  const cokArray = cokDecode.split(";");
+  var resultCok = null;
+
+  cokArray.forEach((el) => {
+    console.log(el);
+    if (el.indexOf(name) == 0) {
+      console.log(el, "< = element");
+      resultCok = el.substring(name.length + 1);
+      console.log(name, "< = Name");
+      console.log(resultCok, "< = Value");
+    }
+  });
+  return resultCok;
+}
+function deleteCookie(name) {
+  createCookie(name, null, null);
+}
